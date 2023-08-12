d071ce211c8c4334b389d6ae4ec372244be5b4e0
James Hines <jameshines10@gmail.com>
2023-08-12 20:42:41 +0000

    These changes were made in order to deploy the pet shop app to the Goerli test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+
+const privateKeys = [
+  process.env.PRIVATE_KEY_0,
+  process.env.PRIVATE_KEY_1,
+];
+
+const infuraKey = process.env.INFURA_KEY;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +20,14 @@ module.exports = {
     },
     develop: {
       port: 9545
-    }
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(process.env.SECRET_KEY, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 5, // Goerli's id
+      gas: 5500000, // Gas limit - set it to your contract needs
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
   }
 };
